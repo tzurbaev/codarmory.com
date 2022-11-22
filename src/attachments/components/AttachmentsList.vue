68793952d70c77258b91ce5eafa892b8fabc8590
tzurbaev <zurbaev@gmail.com>
2022-11-22 20:06:17 +0800

    Update AttachmentsList.vue

--- src/attachments/components/AttachmentsList.vue
+++ src/attachments/components/AttachmentsList.vue
@@ -11,13 +11,13 @@
         </template>
         <template v-else>{{ attachments.length }} attachments</template>
       </p>
-    </div>
 
-    <div class="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 lg:gap-8">
-      <AttachmentGridCard v-for="attachment in attachments"
-                          :key="`AttachmentGridCard-${attachment.id}`"
-                          :attachment="attachment"
-      />
+      <div class="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 lg:gap-8">
+        <AttachmentGridCard v-for="attachment in attachments"
+                            :key="`AttachmentGridCard-${attachment.id}`"
+                            :attachment="attachment"
+        />
+      </div>
     </div>
   </div>
 </template>
